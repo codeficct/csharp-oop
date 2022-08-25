f398cef88d97f3fa553584a80b664bab2032b710
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-24 23:30:23 -0400

    feat(root): add class to clients

---
+++ Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_learn {
+    public class Client {
+        public string name { get; set; }
+        public string lastNname { get; set; }
+        public int phone { get; set; }
+        public string creditCard { get; set; }
+
+        public string FullName {
+            get { return name + " " + lastNname; }
+        }
+    }
+}
